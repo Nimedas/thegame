58b0b9048591a90af450282ac844842a27600960
PALUD Sébastien <palud.e1401191@etud.univ-ubs.fr>
2016-10-03 11:59:24 +0200

    Adding existing files

---
+++ NonPlayableCharacter.java
@@ -0,0 +1,8 @@
+
+public class NonPlayableCharacter extends Player{
+
+	public NonPlayableCharacter(String name, String title, String description, Type type) {
+		super(name, title, description, type);
+	}
+
+}
---
+++ PickableObject.java
@@ -0,0 +1,8 @@
+
+public class PickableObject extends Entity{
+
+	public PickableObject(String name, String title, String description, Type type) {
+		super(name, title, description, type);
+	}
+
+}
