5e6efcbb444fb8c301f2bd7636149296424db864
PALUD Sébastien <palud.e1401191@etud.univ-ubs.fr>
2016-10-03 12:06:10 +0200

    fixing things

--- NonPlayableCharacter.java
+++
@@ -1,8 +0,0 @@
-
-public class NonPlayableCharacter extends Player{
-
-	public NonPlayableCharacter(String name, String title, String description, Type type) {
-		super(name, title, description, type);
-	}
-
-}
--- PickableObject.java
+++
@@ -1,8 +0,0 @@
-
-public class PickableObject extends Entity{
-
-	public PickableObject(String name, String title, String description, Type type) {
-		super(name, title, description, type);
-	}
-
-}
